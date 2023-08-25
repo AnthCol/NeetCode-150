774bd2bf775a6dd853da6ceb8fc53637abf02dd8
Anthony Colaiacovo <82617912+AnthCol@users.noreply.github.com>
2023-08-24 21:30:27 -0400

    Create validParentheses.cpp

---
+++ validParentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st; 
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (!st.empty())
+            {
+                if (s[i] == ')' && st.top() == '(') st.pop(); 
+                else if (s[i] == ']' && st.top() == '[') st.pop(); 
+                else if (s[i] == '}' && st.top() == '{') st.pop(); 
+                else st.push(s[i]); 
+            }
+            else
+            {   
+                st.push(s[i]); 
+            }
+        }
+
+
+        return (st.empty()); 
+    }
+};
